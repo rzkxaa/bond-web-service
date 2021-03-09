ff2f7d0436c212eeeca0831792e0d0ec7efb8794
rzkxaa <ramadhantirizkika@yahoo.com>
2021-03-08 18:32:22 -0800

    done

---
+++ data/pokemon-type.js
@@ -0,0 +1,184 @@
+{
+	"title": "Pokemon Sorted by Primary Type",
+	"pokemon": [
+        {
+            "Name": "Caterpie",
+            "PokedexNumber": 10,
+            "Generation": 1,
+            "PrimaryType": "Bug",
+            "SecondaryType": "N/A",
+            "Category": "Worm Pokemon",
+            "Evolution": "Caterpie > Metapod > Butterfree",
+            "Legendary": "No",
+			"Image": "caterpie.png"
+        },
+
+        {
+            "Name": "Metapod",
+            "PokedexNumber": 11,
+            "Generation": 1,
+            "PrimaryType": "Bug",
+            "SecondaryType": "N/A",
+            "Category": "Cocoon Pokemon",
+            "Evolution": "Caterpie > Metapod > Butterfree",
+            "Legendary": "No",
+			"Image": "metapod.png"
+        },
+
+        {
+            "Name": "Butterfree",
+            "PokedexNumber": 12,
+            "Generation": 1,
+            "PrimaryType": "Bug",
+            "SecondaryType": "Flying",
+            "Category": "Butterfly Pokemon",
+            "Evolution": "Caterpie > Metapod > Butterfree",
+            "Legendary": "No",
+			"Image": "butterfree.png"
+        },
+
+        {
+            "Name": "Weedle",
+            "PokedexNumber": 13,
+            "Generation": 1,
+            "PrimaryType": "Bug",
+            "SecondaryType": "Poison",
+            "Category": "Hairy Bug Pokemon",
+            "Evolution": "Weedle > Kakuna > Beedrill",
+            "Legendary": "No",
+			"Image": "weedle.png"
+        },
+
+        {
+            "Name": "Kakuna",
+            "PokedexNumber": 14,
+            "Generation": 1,
+            "PrimaryType": "Bug",
+            "SecondaryType": "Poison",
+            "Category": "Cocoon Pokemon",
+            "Evolution": "Weedle > Kakuna > Beedrill",
+            "Legendary": "No",
+			"Image": "kakuna.png"
+        },
+
+        {
+            "Name": "Beedrill",
+            "PokedexNumber": 15,
+            "Generation": 1,
+            "PrimaryType": "Bug",
+            "SecondaryType": "Poison",
+            "Category": "Poison Bee Pokemon",
+            "Evolution": "Weedle > Kakuna > Beedrill",
+            "Legendary": "No",
+			"Image": "beedrill.png"
+        },
+
+        {
+            "Name": "Bulbasaur",
+            "PokedexNumber": 1,
+            "Generation": 1,
+            "PrimaryType": "Grass",
+            "SecondaryType": "Poison",
+            "Category": "Seed Pokemon",
+            "Evolution": "Bulbasaur > Ivysaur > Venasaur",
+            "Legendary": "No",
+            "Image": "bulbasaur.png"
+        },
+
+        {
+            "Name": "Ivysaur",
+            "PokedexNumber": 2,
+            "Generation": 1,
+            "PrimaryType": "Grass",
+            "SecondaryType": "Poison",
+            "Category": "Seed Pokemon",
+            "Evolution": "Bulbasaur > Ivysaur > Venasaur",
+            "Legendary": "No",
+            "Image": "ivysaur.png"
+        },
+
+        {
+            "Name": "Venusaur",
+            "PokedexNumber": 3,
+            "Generation": 1,
+            "PrimaryType": "Grass",
+            "SecondaryType": "Poison",
+            "Category": "Seed Pokemon",
+            "Evolution": "Bulbasaur > Ivysaur > Venasaur",
+            "Legendary": "No",
+            "Image": "venusaur.png"
+        },
+
+        {
+            "Name": "Squirtle",
+            "PokedexNumber": 7,
+            "Generation": 1,
+            "PrimaryType": "Water",
+            "SecondaryType": "N/A",
+            "Category": "Tiny Turtle Pokemon",
+            "Evolution": "Squirtle > Wartortle > Blastoise",
+            "Legendary": "No",
+			"Image": "squirtle.png"
+        },
+
+        {
+            "Name": "Wartortle",
+            "PokedexNumber": 8,
+            "Generation": 1,
+            "PrimaryType": "Water",
+            "SecondaryType": "N/A",
+            "Category": "Turtle Pokemon",
+            "Evolution": "Squirtle > Wartortle > Blastoise",
+            "Legendary": "No",
+			"Image": "wartortle.png"
+        },
+
+        {
+            "Name": "Blastoise",
+            "PokedexNumber": 9,
+            "Generation": 1,
+            "PrimaryType": "Water",
+            "SecondaryType": "N/A",
+            "Category": "Shellfish Pokemon",
+            "Evolution": "Squirtle > Wartortle > Blastoise",
+            "Legendary": "No",
+			"Image": "blastoise.png"
+        },
+
+        {
+            "Name": "Charmander",
+            "PokedexNumber": 4,
+            "Generation": 1,
+            "PrimaryType": "Fire",
+            "SecondaryType": "N/A",
+            "Category": "Lizard Pokemon",
+            "Evolution": "Charmander > Charmeleon > Charizard",
+            "Legendary": "No",
+			"Image": "charmander.png"
+        },
+
+        {
+            "Name": "Charmeleon",
+            "PokedexNumber": 5,
+            "Generation": 1,
+            "PrimaryType": "Fire",
+            "SecondaryType": "N/A",
+            "Category": "Lizard Pokemon",
+            "Evolution": "Charmander > Charmeleon > Charizard",
+            "Legendary": "No",
+			"Image": "charmeleon.png"
+        },
+
+        {
+            "Name": "Charizard",
+            "PokedexNumber": 6,
+            "Generation": 1,
+            "PrimaryType": "Fire",
+            "SecondaryType": "Flying",
+            "Category": "Lizard Pokemon",
+            "Evolution": "Charmander > Charmeleon > Charizard",
+            "Legendary": "No",
+			"Image": "charizard.png"
+        }
+	]
+}
